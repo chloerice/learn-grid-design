17880156d807ca285aeb4cb1bd079943953e3c55
Chloe Rice <chloe.mi.rice@gmail.com>
2017-05-27 20:19:52 -0400

    devops - setup and deployed to firebase

---
+++ src/flex/FlexLevel0.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './flex.css'
+
+export default class Flex extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="Flex">
+
+      </div>
+    )
+  }
+}
---
+++ src/levels.js
@@ -0,0 +1,11 @@
+import GridLevel0 from './grid/GridLevel0'
+import FlexLevel0 from './flex/FlexLevel0'
+
+export const levels = {
+  flex: {
+    0: FlexLevel0
+  },
+  grid: {
+    0: GridLevel0
+  }
+}
